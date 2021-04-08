0b52aa5969e70bbdcb66bfc74372c7ced988e69f
chrysmur <sostomc011@gmail.com>
2021-04-08 20:38:09 +0300

    dockerfile refactor

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to angry pandas!
+          We are a group of nerds
         </p>
       </header>
     </div>
