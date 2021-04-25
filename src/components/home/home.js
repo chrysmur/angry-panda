a22adc5e2419720f795b1364012f836c1ab32ecb
chrysmur <sostomc011@gmail.com>
2021-04-25 19:35:51 +0300

    left for animation

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,14 +13,14 @@ class Home extends Component {
                         <Card.Title><h2 className="text-info text-center"> Angry Panda Developer's Hub</h2></Card.Title>
                     
                         <Card.Text className="text-center">
-                        <i className="fas fa-walking"></i>This is our Journey 
+                        <i className="fas fa-code"></i> {"<<<Left for animation>>>"}
                         </Card.Text>
                     </Card.Body>
                 </Card>
 
                 <Row>
                     <Col>
-                        <Card className="bg-secondary">
+                        <Card className="bg-transparent">
                             <Card.Body>
                             <Card.Title><strong>Languages</strong></Card.Title>
                             <Card.Text>Python, Javascript, Go, Elixir, PHP </Card.Text>
@@ -40,12 +40,20 @@ class Home extends Component {
                         </Card>
                     </Col>
                     <Col>
-                    <Card className="bg-secondary">
+                    <Card className="bg-transparent">
                             <Card.Body>
                             <Card.Title><strong>Journeys</strong></Card.Title>
                             <Card.Text> We have been doing this stuff for a varying number years in various fields, and we are not done yet...</Card.Text>
                             <hr/>
                             <Card.Text> See our individual journeys <a href="journey">here</a></Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card className="bg-transparent">
+                            <Card.Body>
+                            <Card.Title><strong>Rookie Corner</strong></Card.Title>
+                            <Card.Text> Yeah, it can be tough to start</Card.Text>
+                            <hr/>
+                            <Card.Text> <a href="journey">Reach out</a>, and we will help you find the solution </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
