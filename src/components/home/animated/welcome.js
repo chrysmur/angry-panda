c309449bd3de72c27f87760038ce918f6af04f80
chrysmur <sostomc011@gmail.com>
2021-04-26 23:12:39 +0300

    animations

---
+++ src/components/home/animated/welcome.js
@@ -0,0 +1,10 @@
+const spinner = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏']
+
+const termLines = [  
+  {
+    text: "@angry-panda> npm run start",
+    cmd: true,
+  },
+]
+
+export default termLines
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import {Card, Row, Col } from 'react-bootstrap'
+import TextAnimation from 'react-animate-text';
+
+
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -13,7 +16,12 @@ class Home extends Component {
                         <Card.Title><h2 className="text-info text-center"> Angry Panda Developer's Hub</h2></Card.Title>
                     
                         <Card.Text className="text-center">
-                        <i className="fas fa-code"></i> {"<<<Left for animation>>>"}
+                            <div style={{background:'black'}}>
+                                <TextAnimation>
+                                    <span style = {{ color:"yellow"}}>$ </span>
+                                    <strong style={{ color:'green' }}>npm run start</strong>
+                                </TextAnimation>
+                            </div>
                         </Card.Text>
                     </Card.Body>
                 </Card>
@@ -22,38 +30,44 @@ class Home extends Component {
                     <Col>
                         <Card className="bg-transparent">
                             <Card.Body>
-                            <Card.Title><strong>Languages</strong></Card.Title>
-                            <Card.Text>Python, Javascript, Go, Elixir, PHP </Card.Text>
-                            <hr/>
-                            <Card.Title><strong>Framework and Runtime Environments</strong></Card.Title>
-                            <Card.Text>NodeJS, ReactJS, Django, Flask, Phoenix, Laravel</Card.Text>
-                            <hr/>
-                            <Card.Title><strong>Databases / KV stores</strong></Card.Title>
-                            <Card.Text>Postgresql, Mysql, MongoDB, Cassandra, Redis </Card.Text>
-                            <hr/>
-                            <Card.Title><strong>Design tools</strong></Card.Title>
-                            <Card.Text>Redis, Apache Kafka, RabbitMQ  </Card.Text>
-                            <hr/>
-                            <Card.Title><strong>Others</strong></Card.Title>
-                            <Card.Text>ML, Data structures and <a href="blog">much more </a>  </Card.Text>
+                            <TextAnimation charInterval='20'>   
+                                <Card.Title><strong className="text-info">Languages</strong></Card.Title>
+                                <Card.Text>Python, Javascript, Go, Elixir, PHP </Card.Text>
+                                <hr/>
+                                <Card.Title><strong className="text-info">Framework and Runtime Environments</strong></Card.Title>
+                                <Card.Text>NodeJS, ReactJS, Django, Flask, Phoenix, Laravel</Card.Text>
+                                <hr/>
+                                <Card.Title><strong className="text-info">Databases / KV stores</strong></Card.Title>
+                                <Card.Text>Postgresql, Mysql, MongoDB, Cassandra, Redis </Card.Text>
+                                <hr/>
+                                <Card.Title><strong className="text-info">Design tools</strong></Card.Title>
+                                <Card.Text>Redis, Apache Kafka, RabbitMQ  </Card.Text>
+                                <hr/>
+                                <Card.Title><strong className="text-info">Others</strong></Card.Title>
+                                <Card.Text>ML, Data structures and <a href="blog">much more </a>  </Card.Text>
+                            </TextAnimation>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col>
                     <Card className="bg-transparent">
                             <Card.Body>
-                            <Card.Title><strong>Journeys</strong></Card.Title>
-                            <Card.Text> We have been doing this stuff for a varying number years in various fields, and we are not done yet...</Card.Text>
-                            <hr/>
-                            <Card.Text> See our individual journeys <a href="journey">here</a></Card.Text>
+                            <TextAnimation charInterval="30">
+                                <Card.Title><strong className="text-info">Journeys</strong></Card.Title>
+                                <Card.Text> We have been doing this stuff for a varying number years in various fields, and we are not done yet...</Card.Text>
+                                <hr/>
+                                <Card.Text> See our individual journeys <a href="journey">here</a></Card.Text>
+                            </TextAnimation>
                             </Card.Body>
                         </Card>
                         <Card className="bg-transparent">
                             <Card.Body>
-                            <Card.Title><strong>Rookie Corner</strong></Card.Title>
-                            <Card.Text> Yeah, it can be tough to start</Card.Text>
-                            <hr/>
-                            <Card.Text> <a href="journey">Reach out</a>, and we will help you find the solution </Card.Text>
+                            <TextAnimation charInterval="">
+                                <Card.Title><strong className="text-info">Rookie Corner</strong></Card.Title>
+                                <Card.Text> Yeah, it can be tough to start</Card.Text>
+                                <hr/>
+                                <Card.Text> <a href="journey">Reach out</a>, and we will help you find the solution </Card.Text>
+                            </TextAnimation>
                             </Card.Body>
                         </Card>
                     </Col>
